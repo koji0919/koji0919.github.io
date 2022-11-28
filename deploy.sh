03fa60812299996609f90a77ee00fc15a3a377a9
安田 航大 <yasuda_kodai@sakaimed.local>
2022-11-28 20:01:07 +0900

    pass_test

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ sleep 0.1
 echo -e "\033[0;32mCopy WEB data...\033[0m"
 rm -rf ../docs/*
 cp -R ./public/* ../docs/28e51044f4a9cbae2bbd3d8a9d8c902ad1455d42208277ac4a913b003038a3dc
-cd -R ./4password/* ..docs/
+cp -R ./4password/* ../docs/
 sleep 0.1
 
 echo -e "\033[0;32mNow Uploading...\033[0m"
