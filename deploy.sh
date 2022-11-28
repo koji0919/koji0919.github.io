7d7df0f28deb2316e01319d96992a262092d775d
安田 航大 <yasuda_kodai@sakaimed.local>
2022-11-28 22:35:43 +0900

    test_

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,9 @@ sleep 0.1
 echo -e "\033[0;32mCopy WEB data...\033[0m"
 rm -rf ../docs/*
 mkdir ../docs/1be9abc2a2d91b83142e9ae8474bfa99d9905c2357ad367cbbee2d4a41037781
-cp -R ./public/* ../docs/1be9abc2a2d91b83142e9ae8474bfa99d9905c2357ad367cbbee2d4a41037781/
+cp -R ./public/* ../docs/1be9abc2a2d91b83142e9ae8474bfa99d9905c2357ad367cbbee2d4a41037781
 # cp -R ./public/* ../docs/
-cp -R ./4password/* ../docs/
+cp -R ./4password/* ../docs
 sleep 0.1
 
 echo -e "\033[0;32mNow Uploading...\033[0m"
